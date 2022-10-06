273ebcf1f7ec111648014a69aebdf02b8a4e49e5
emmanuelonchiri <115180199+emmanuelonchiri@users.noreply.github.com>
2022-10-06 23:05:39 +0300

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void)
+{
+   int x;
+   int y;
+   int total;
+   x=4;
+   y=4;
+   total=x+y;
+   printf("%d,",total);
+   total=x-y;
+   printf("%d,",total);
+   total=x%y;
+   printf("%d,",total);
+   total=x*y;
+   printf("%d,",total);
+   return 0;
+}
